0187911eaf5a1bbdaf5977ec1b2f326254ac33a2
areum1099 <areum1099@gmail.com>
2025-08-17 14:44:49 +0900

    ChatPrototypeAppTest.swift 커밋

---
+++ ChatPrototypeAppTest.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by areum Lee on 3/22/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
